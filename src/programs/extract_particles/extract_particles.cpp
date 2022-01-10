dff25275ce0075cc23b37341a8f44df8357f6a99
kekexin714 <kekexin714@gmail.com>
2022-01-09 21:31:07 -0500

    consistent with template matching coordinate outputs

--- src/programs/extract_particles/extract_particles.cpp
+++ src/programs/extract_particles/extract_particles.cpp
@@ -27,16 +27,17 @@ void ExtractParticlesApp::DoInteractiveUserInput()
 	wxString	coordinates_filename				=	my_input->GetFilenameFromUser("Coordinates (PLT) filename","The input particle coordinates, in Imagic-style PLT forlmat","coos.plt",true);
 	wxString	output_stack_filename				=	my_input->GetFilenameFromUser("Filename for output stack of particles.","A stack of particles will be written to disk","particles.mrc",false);
 	int			output_stack_box_size				=	my_input->GetIntFromUser("Box size for output candidate particle images (pixels)","In pixels. Give 0 to skip writing particle images to disk.","256",0);
-
+	float       pixel_size 							=   my_input->GetFloatFromUser("pixel size","pixel size","1.0",0.1,100.0);
 
 
 	delete my_input;
 
-	my_current_job.Reset(4);
-	my_current_job.ManualSetArguments("ttti",			micrograph_filename.ToStdString().c_str(),
+	my_current_job.Reset(5);
+	my_current_job.ManualSetArguments("tttif",			micrograph_filename.ToStdString().c_str(),
 														coordinates_filename.ToStdString().c_str(),
 														output_stack_filename.ToStdString().c_str(),
-														output_stack_box_size
+														output_stack_box_size,
+														pixel_size
 														);
 
 
@@ -55,6 +56,7 @@ bool ExtractParticlesApp::DoCalculation()
 	wxString 	coordinates_filename 						= 	my_current_job.arguments[1].ReturnStringArgument();
 	wxString	output_stack_filename						=	my_current_job.arguments[2].ReturnStringArgument();
 	int			output_stack_box_size						=	my_current_job.arguments[3].ReturnIntegerArgument();
+	float		pixel_size						            =	my_current_job.arguments[4].ReturnFloatArgument();
 
 
 
@@ -65,24 +67,30 @@ bool ExtractParticlesApp::DoCalculation()
 
 	// Let's box particles out
 	Image micrograph;
+	Image micrograph_copy;
 	Image box;
 	box.Allocate(output_stack_box_size,output_stack_box_size,1,true);
 	micrograph.ReadSlice(&micrograph_file,1);
+	micrograph_copy.Allocate(micrograph.logical_x_dimension, micrograph.logical_y_dimension, 1);
 	float micrograph_mean = micrograph.ReturnAverageOfRealValues();
 	NumericTextFile *input_coos_file;
 	input_coos_file = new NumericTextFile(coordinates_filename, OPEN_TO_READ, 3);
 	int number_of_particles = input_coos_file->number_of_lines;
 	MRCFile output_stack;
-	output_stack.OpenFile(output_stack_filename.ToStdString(),true);
+	//output_stack.OpenFile(output_stack_filename.ToStdString(),true);
 	my_progress_bar = new ProgressBar(number_of_particles);
 	float plt_x, plt_y;
-	float my_x, my_y;
-	float temp_array[3];
+	int my_x, my_y;
+	float temp_array[8];
 	for ( int counter = 0; counter < number_of_particles; counter ++ )
 	{
 		input_coos_file->ReadLine(temp_array);
-		plt_x = temp_array[0];
-		plt_y = temp_array[1];
+		plt_x = roundf(temp_array[3]/pixel_size);  // convert A to pixels in coordinate file
+		//wxPrintf("pixel size = %f\n", pixel_size);
+		wxPrintf("old and new plt_x = %f, %f\n", temp_array[3], plt_x);
+
+		plt_y = roundf(temp_array[4]/pixel_size);  // convert A to pixels in coordinate file
+		wxPrintf("old and new plt_y = %f, %f\n", temp_array[4], plt_y);
 		/*
 		 * plt_x = (my_y + phys_addr_box_center_y) + 1.0;
 		 * plt_y = (logical_x_dim - (phys_addr_box_center_x + my_x)) + 1.0;
@@ -90,12 +98,20 @@ bool ExtractParticlesApp::DoCalculation()
 		 * my_x = -1 * ((plt_y - 1.0)  - logical_x_dim + phys_addr_box_center_x)
 		 * my_y = (plt_x - 1.0)  - phys_addr_box_center_y;
 		 */
-		my_x = (-1.0) * ((plt_y - 1.0) - micrograph.logical_x_dimension + micrograph.physical_address_of_box_center_x);
-		my_y = (plt_x - 1.0) - micrograph.physical_address_of_box_center_y;
-		micrograph.ClipInto(&box,micrograph_mean,false,1.0,-int(my_x),-int(my_y),0);
-		box.WriteSlice(&output_stack , counter + 1);
+		//my_x = (-1.0) * ((plt_y - 1.0) - micrograph.logical_x_dimension + micrograph.physical_address_of_box_center_x);
+		//my_y = (plt_x - 1.0) - micrograph.physical_address_of_box_center_y;
+		my_x = int(plt_x-micrograph.physical_address_of_box_center_x);
+		my_y = int(plt_y-micrograph.physical_address_of_box_center_y);
+		//wxPrintf("my_x = %i\n", my_x);
+		//wxPrintf("my_y = %i\n", my_y);
+		micrograph_copy.CopyFrom(&micrograph);
+		micrograph_copy.RealSpaceIntegerShift(plt_x-micrograph.physical_address_of_box_center_x, plt_y-micrograph.physical_address_of_box_center_y);
+		micrograph_copy.ClipInto(&box);
+		//micrograph.ClipInto(&box,micrograph_mean,false,1.0,-int(plt_x-micrograph.physical_address_of_box_center_x),-int(plt_y-micrograph.physical_address_of_box_center_y),0);
+		//box.WriteSlice(&output_stack , counter + 1);
 
-		//
+		box.QuickAndDirtyWriteSlice(wxString::Format("/groups/kexin/research/scaling_mip/normality_test/real_image_147/windowed_peak_%i.mrc", counter).ToStdString(),1);
+
 		my_progress_bar->Update(counter+1);
 	}
 	delete my_progress_bar;
